fc9055793ca915e7b838f574e459df0c03f38cbb
Luel <prince123zegreat@gmail.com>
2019-11-15 13:31:44 +0300

    finds cubic root using newtonian method

--- Cube-Root-by-Newtons-Method.scm
+++ Cube-Root-by-Newtons-Method.scm
@@ -1,13 +1,12 @@
-(define (check1 n guess iteration)
+(define (cube_root n guess iteration)
 (if (= 0 iteration) guess 
-	(check1 n 
+	(cube_root n 
 		(/ (+ (* 2 guess) (/ n (* guess guess))) 3.0)
 	 (- iteration 1))))
 
 
 
-(define (cube_root n guess iteration)
-(check1 n guess iteration))
+
 
 
 ; as we increse the number of iteration the accuracy will also increse
@@ -19,6 +18,23 @@ $1 = 2.0
 $2 = 4.64158883361313
 (cube_root 1 1 10)
 $3 = 1.0
+
+; without using iteration(using episolon)
+(define (cube_root n guess)
+(if (> 0.0000001 (abs(- n (* guess guess guess)))) guess 
+	(cube_root n 
+		(/ (+ (* 2 guess) (/ n (* guess guess))) 3.0))))
+
+; sample inputs
+(cube_root 8 1)
+$1 = 2.000000000012062
+(cube_root 100 1 10)
+$2 = 4.64158883361313
+(cube_root 1 1 10)
+$3 = 1.0
+
+
+
 
 
 
