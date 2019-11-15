9cbcd2d17df68a71bb25dff227fede6cfab2d93e
Luel <prince123zegreat@gmail.com>
2019-11-15 13:30:15 +0300

    finds square root using newtonian method

--- Square-Roots-by-Newtons-Method.scm
+++ Square-Roots-by-Newtons-Method.scm
@@ -1,15 +1,11 @@
 
-(define (check n guess iteration)
-(if (= 0 iteration) guess (check n (/ (+ (/ n (* 1.0 guess)) guess) 2.0) (- iteration 1))))
-
 (define (Square_root n guess iteration)
-(check n guess iteration))
+(if (= 0 iteration) guess (Square_root n (/ (+ (/ n (* 1.0 guess)) guess) 2.0) (- iteration 1))))
 
 ; as we increse the number of iteration the accuracy will also increse
 
 
 ; sample inputs
-~~~
 (Square_root 100 0.5 6)
 $1 = 10.033108838737885
 (Square_root 100 0.5 10)
@@ -18,7 +14,22 @@ $2 = 10.0
 $3 = 3.0
 (Square_root 2 1 10)
 $4 = 1.414213562373095
-~~~
+
+
+
+; with out using iteration
+(define (Square_root n guess)
+(if (> 0.0000001 (abs(- n (* guess guess)))) guess 
+	(Square_root n (/ (+ (/ n (* 1.0 guess)) guess) 2.0))))
+
+
+; sample inputs
+(Square_root 100 0.5 6)
+$1 = 10.000000000149214
+(Square_root 9 2 7)
+$2 = 3.0000000000393214
+(Square_root 2 1 10)
+$3 = 1.4142135623746899
 
 
 
