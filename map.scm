c613544e180aaa01495b5b1d98501a0cfde2601c
Luel <prince123zegreat@gmail.com>
2019-11-20 14:27:10 +0000

    how map works in lists

--- map.scm
+++ map.scm
@@ -4,3 +4,7 @@
         '()
         (cons (f (car l))
               (map f (cdr l))))))
+
+; sample inputes
+(map (lambda (x) (* x 10)) '(1 2 3 4 5)) ====> (10 20 30 40 50)
+(map (lambda (x) (square x)) '(1 2 3 4 5))===> (1 4 9 16 25)
